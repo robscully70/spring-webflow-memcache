1da7fec3f729d5e37514c14ecb9d6c10e5d91fea
Tiris Valinlore <tvalinl@us.ibm.com>
2015-02-04 12:22:14 -0500

    adding changes to the flow to support logging things. and added a few different modes to test

--- README.md
+++ README.md
@@ -1,22 +1,84 @@
+Welcome:
+====================
+This project is to demonstrate an Issue I am having with memcache (or more likely the connector from [memcached-session-manager](https://code.google.com/p/memcached-session-manager/) ).
+
+
 To get the code:
--------------------
+====================
 Clone the repository:
 
     $ git clone git://github.com/tyrinslys/spring-webflow-memcache.git
 
 If this is your first time using Github, review http://help.github.com to learn the basics.
 
+Setup
+====================
+You will need to have Maven, Tomcat 7, and memcached installed prior to anything else.
+
+It is important you setup Tomcat 7 rather than using the embedded Maven Tomcat.
+
+Quick reference:
+1. create Tomcat user for delpoyment with Maven
+1. copy context from project to Tomcat
+1. add settings to Maven for Tomcat deployment user
+
+Tomcat User Setup
+-----------------
+In at apache-tomcat-7.0.57/conf/tomcat-users.xml add user:
+
+    <user username="deploymentUser" password="deploy12345password" roles="manager-gui,manager-script"/>
+
+Also add roles if they are not in already:
+
+    <role rolename="manager-gui"/>
+    <role rolename="manager-script"/>
+
+Tomcat Context Setup
+--------------------
+Copy file `src/main/webapp/WEB-INF/context.xml` to `apache-tomcat-7.0.57/conf/context.xml`  
+
+Maven Server Profile Settings
+-----------------------------
+In your `.m2/settings.xml` file add the following in your <servers> tag
+
+    <server>
+      <id>local-tomcat</id>
+      <username>deploymentUser</username>
+      <password>deploy12345password</password>
+    </server>
+
 To run the application:
--------------------	
-From the command line with Maven:
+====================
+1. Start memcached:
 
-    $ cd spring-webflow-memcache
-    $ mvn tomcat7:run [-Dmaven.tomcat.port=<port no.>] (In case 8080 is busy] 
+        $ memcached -d -m 30 -l 127.0.0.1 -p 11211
 
-Access the deployed web application at: http://localhost:8080/spring-mvc-showcase/
-The webflow test is at http://localhost:8080/spring-mvc-showcase/flow/testFlow
+1. Start Tomcat:
+
+        $ apache-tomcat-7.0.57/bin/startup.sh
+
+1. From the command line with Maven:
+
+        $ cd spring-webflow-memcache
+        $ mvn clean -DskipTests=true tomcat7:deploy
+
+    And to redeploy run:
+
+        $ mvn mvn clean -DskipTests=true tomcat7:redeploy
+
+1. The webflow test is at [http://localhost:8080/spring-mvc-showcase/flow/testFlow?debug=grow](http://localhost:8080/spring-mvc-showcase/flow/testFlow?debug=grow)
+
+Other Test Modes
+----------------
+by default the grow strategy is used. This can be changed with a debug variable that is intended to be set at the start of the flow.
+
+* grow -[http://localhost:8080/spring-mvc-showcase/flow/testFlow?debug=grow](http://localhost:8080/spring-mvc-showcase/flow/testFlow?debug=grow)
+* static size, unchangeing [http://localhost:8080/spring-mvc-showcase/flow/testFlow?debug=static-nonchanging](http://localhost:8080/spring-mvc-showcase/flow/testFlow?debug=static-nonchanging)
+* static size, changeing [http://localhost:8080/spring-mvc-showcase/flow/testFlow?debug=static-changing](http://localhost:8080/spring-mvc-showcase/flow/testFlow?debug=static-changing)
 
 Spring MVC Showcase
--------------------
+====================
 
 This project was forked from [spring-mvc-showcase](https://github.com/spring-projects/spring-mvc-showcase) and I didn't disable anything from that project.
+
+You can still access the deployed web application at: [http://localhost:8080/spring-mvc-showcase/](http://localhost:8080/spring-mvc-showcase/)
--- src/main/java/org/springframework/samples/mvc/webflow/SimpleWebFlowController.java
+++ src/main/java/org/springframework/samples/mvc/webflow/SimpleWebFlowController.java
@@ -1,34 +1,120 @@
 package org.springframework.samples.mvc.webflow;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Random;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.math.RandomUtils;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.context.Theme;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.support.RequestContextUtils;
+import org.springframework.webflow.execution.repository.FlowExecutionRestorationFailureException;
 import org.springframework.webflow.executor.FlowExecutor;
 import org.springframework.webflow.mvc.servlet.FlowController;
 
 @Controller
-public class SimpleWebFlowController  extends FlowController {
+public class SimpleWebFlowController extends FlowController {
+
+	private static final String SESSION_DEBUG = "session.debug";
+	private static final String SESSION_MAP = "session.map";
+	Logger logger = Logger.getLogger(this.getClass());
+	Random random = new Random();
+
+	@RequestMapping("/flow/*Flow")
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response, @RequestParam(required = false) String debug) {
+		if (!StringUtils.isBlank(debug)) {
+			request.getSession().setAttribute(SESSION_DEBUG, debug);
+			request.getSession().removeAttribute(SESSION_MAP);
+		}
+		String sessionDebug = (String) request.getSession().getAttribute(SESSION_DEBUG);
+		sessionDebug = StringUtils.trimToEmpty(sessionDebug);
+		logger.info("sessionDebug is:" + sessionDebug);
+
+		Theme theme = RequestContextUtils.getTheme(request);
+		request.getSession().setAttribute("session.theme", theme);
+
+		Map<Integer, Integer> sessionMap = getSessionMap(request);
+		switch (sessionDebug) {
+		case "static-nonchanging":
+			fillMapToSize(sessionMap, 350);
+			break;
+		case "static-changing":
+			fillMapToSize(sessionMap, 350);
+			changeOneEntry(sessionMap);
+			break;
+		case "growing":
+		default:
+			grow(sessionMap);
+			break;
+		}
+		logger.info("about to look up the flow sessionMap size is: " + sessionMap.size());
+		return handleRequest(request, response);
+	}
 
 	@Override
-	@RequestMapping("/flow/*Flow")
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) {
 		try {
 			return super.handleRequest(request, response);
+		} catch (FlowExecutionRestorationFailureException e) {
+			// TODO: print details about map
+			// Map<Integer, Integer> sessionMap = getSessionMap(request);
+			logger.error("Flow Restoration issue.", e);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			logger.error("General Trouble handling flow.", e);
 		}
 		return null;
 	}
-	
+
+	private void changeOneEntry(Map<Integer, Integer> sessionMap) {
+		Iterator<Entry<Integer, Integer>> iterator = sessionMap.entrySet().iterator();
+		iterator.next();
+		iterator.remove();
+		grow(sessionMap);
+	}
+
+	private void fillMapToSize(Map<Integer, Integer> map, int size) {
+		if (map.size() < size) {
+			int addAmount = size - map.size();
+			for (; addAmount > 0; addAmount--) {
+				grow(map);
+			}
+			logger.info("filled map size is now" + map.size());
+		}
+	}
+
+	private void grow(Map<Integer, Integer> sessionMap) {
+		sessionMap.put(RandomUtils.nextInt(random), RandomUtils.nextInt(random));
+		logger.info("Added Integer pair size is now: " + sessionMap.size());
+	}
+
+	@SuppressWarnings("unchecked")
+	private Map<Integer, Integer> getSessionMap(HttpServletRequest request) {
+		Object sessionMap = request.getSession().getAttribute(SESSION_MAP);
+		if (sessionMap == null || !(sessionMap instanceof Map)) {
+			sessionMap = new HashMap<Integer, Integer>();
+			request.getSession().setAttribute(SESSION_MAP, sessionMap);
+		}
+		return (Map<Integer, Integer>) sessionMap;
+	}
+
 	@Resource
 	@Override
 	public void setFlowExecutor(FlowExecutor flowExecutor) {
 		super.setFlowExecutor(flowExecutor);
 	}
 
+	public static void logCount(Integer count) {
+		Logger.getLogger(SimpleWebFlowController.class).info(String.format("flowScope.counter is now %s", count));
+	}
 }
